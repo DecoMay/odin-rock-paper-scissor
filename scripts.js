4b8463d690e43472c08da7534ac2d528a0df99ac
DecoMay <143063276+DecoMay@users.noreply.github.com>
2023-09-19 17:49:51 +0800

    Add playRound function

--- scripts.js
+++ scripts.js
@@ -1,8 +1,55 @@
 function getComputerChoice() {
-    let validAction = ["Rock", "Paper", "Scissor"];
-    let action = validAction[Math.floor(Math.random(0) * 3)] 
+    let validAction = ["rock", "paper", "scissor"];
+    let action = validAction[Math.floor(Math.random() * 3)] 
     
     return action;
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+    // console.log(playerSelection)
+    let result = '';
+    
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "rock" :
+                result = 'Draw';
+                break;
+            case "paper" :
+                result = 'You Lose';
+                break;
+            case "scissor" :
+                result = 'You Win';
+                break;
+        }
+    } else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "rock" :
+                result = 'You Win';
+                break;
+            case "paper" :
+                result = 'Draw';
+                break;
+            case "scissor" :
+                result = 'You Lose';
+                break;
+        }
+    } else if (playerSelection == "scissor") {
+        switch (computerSelection) {
+            case "rock" :
+                result = 'You Lose';
+                break;
+            case "paper" :
+                result = 'You Win';
+                break;
+            case "scissor" :
+                result = 'Draw';
+                break;
+        }
+    }
+    console.log('You ' +playerSelection + '/ /' + 'COM ' + computerSelection);
+    return result;
+}
+
+// console.log(getComputerChoice())
+console.log(playRound("Rock", getComputerChoice()))
